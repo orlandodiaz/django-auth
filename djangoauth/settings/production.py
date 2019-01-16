e5b30924f9d31eb55e0f642d22a166ca7476babf
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-15 20:09:17 -0600

    Add whitenoise and update requirements

--- djangoauth/settings/production.py
+++ djangoauth/settings/production.py
@@ -12,3 +12,8 @@ STATIC_ROOT = "static/"
 
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
+
+# Static FILES
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
