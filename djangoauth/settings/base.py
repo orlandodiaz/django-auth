521397f52016229835400e245574bf15af6bccea
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-15 20:35:03 -0600

    Move away from google mail and use different email provider

--- djangoauth/settings/base.py
+++ djangoauth/settings/base.py
@@ -124,11 +124,18 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = os.environ.get('DJANGO_MAIL_USERNAME')
+# EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_MAIL_PASSWORD')
+# EMAIL_PORT = 587
+
+
+EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('DJANGO_MAIL_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_MAIL_PASSWORD')
-EMAIL_PORT = 587
-
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
